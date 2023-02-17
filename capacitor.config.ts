7982438f8e7a76748ab3c76573ae27177c200b52
awais awan <awaisawan3104@gmail.com>
2023-02-17 10:32:50 +0400

    App Code

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.chacha.starter',
+  appName: 'Chacha Daig',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
